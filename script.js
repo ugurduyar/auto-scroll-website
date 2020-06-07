e931d9fda1950fcc715b07e5d33489b052380526
ugurduyar <ismailugurduyar@gmail.com>
2020-06-07 23:05:13 +0300

    get random posts added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const filter = document.getElementById("filter");
 let limit = 3;
 let page = 1;
 
+// Fetch posts from api
 async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
@@ -14,3 +15,24 @@ async function getPosts() {
 
   return data;
 }
+
+// Show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+      <div class="number"> ${post.id} </div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+        </div>
+
+        
+      `;
+    postsContainer.appendChild(postEl);
+  });
+}
+
+showPosts();
